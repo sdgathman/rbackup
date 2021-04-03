fb11f778054922cb73d1f1cc89e1d54458b6f2d3
Stuart D. Gathman <stuart@gathman.org>
2021-04-03 00:44:44 -0400

    mount part1 for dos partition table (works for us)

--- lvbackup.sh
+++ lvbackup.sh
@@ -5,7 +5,7 @@ if test "$#" -lt 2; then
 fi
 lvpath="$1"
 media="$2"
-bindir="/var/backup"
+bindir=/usr/libexec/rbackup
 shift 2
 
 if test -e "${media}/BMS_BACKUP_V1"; then
@@ -21,11 +21,24 @@ fi
 set -x
 lvname="${lvpath##*/}"
 lvname="${lvname##*-}"
+vgpath="${lvpath%/${lvname}}"
+vgname="${vgpath##*/}"
 snapname="${lvname}_SNAP"
 snappath="${lvpath}_SNAP"
 tmpdir="/mnt/${snapname}"
 destdir="${media}/${lvname}/current"
 complete="${media}/${lvname}/BMS_BACKUP_COMPLETE"
+
+pttype="$(blkid -o value -s PTTYPE ${lvpath})"
+case "$pttype" in
+"") ;;
+*)  echo "$pttype partitioned LV not yet supported"
+    fspath="/dev/mapper/${vgname}-${snapname}1"
+    echo kpartx -a "${snappath}" 
+    echo "fspath=${fspath}"
+    #exit 1
+    ;;
+esac
 
 if true; then
 # create snapshot of LV
@@ -34,12 +47,20 @@ set -e
 mount -o remount,rw "${media}" 
 mkdir -p "$tmpdir" "$destdir" 
 
-fstype="$(blkid -o value -s TYPE ${snappath})"
+case "$pttype" in
+dos) kpartx -a "${snappath}" || exit 1
+	fspath="/dev/mapper/${vgname}-${snapname}1"
+	;;
+*)	fspath="${snappath}" ;;
+esac
+
+fstype="$(blkid -o value -s TYPE ${fspath})"
 case "$fstype" in
 xfs) opts="ro,noexec,nodev,nouuid";;
 *) opts="ro,noexec,nodev";;
 esac
-mount -o "$opts" "$snappath" "$tmpdir" 
+
+mount -o "$opts" "$fspath" "$tmpdir" 
 rm -f "${complete}"
 fi
 
@@ -50,11 +71,16 @@ if rsync -raHx "$@" "${tmpdir}/" "${destdir}"; then
   [ "$s" != "0" ] && touch "${complete}"
 fi
 umount "$tmpdir"
+
+case "$pttype" in
+dos) kpartx -d "${snappath}" ;;
+esac
+
 if test -e "${complete}"; then
   DT=`date +%y%b%d`
   cd "${media}/${lvname}"
   ${bindir}/rotate.sh $DT
 fi
 mount -r -o remount,ro "${media}"
-/usr/sbin/lvremove  -f "$snappath"
+/usr/sbin/lvremove -f "${snappath}"
 test -e "${complete}"
