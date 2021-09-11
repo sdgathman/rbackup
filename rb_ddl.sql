2b74792b19d1524e5c8d02ae064f212d08501dc4
Stuart D. Gathman <stuart@gathman.org>
2021-09-10 20:30:13 -0400

    Initial DDL for database of backups

---
+++ rb_ddl.sql
@@ -0,0 +1,32 @@
+DROP TABLE rb_vol;
+DROP TABLE rb_source;
+DROP TABLE rb_backup;
+
+CREATE TABLE rb_vol
+(
+    VOL_ID INTEGER NOT NULL,
+    FS_LABEL VARCHAR,	/* backup filesystem label */
+    FS_UUID VARCHAR NOT NULL,
+    PRIMARY KEY (VOL_ID)
+);
+
+CREATE UNIQUE INDEX FS_UUID ON rb_vol (FS_UUID);
+CREATE INDEX FS_LABEL ON rb_vol (FS_LABEL);
+
+CREATE TABLE rb_source	/* backup source */
+(
+    FS_ID INTEGER NOT NULL,
+    LV_NAME VARCHAR,
+    VG_NAME VARCHAR,
+    FS_UUID VARCHAR,	 /* filesystem backed up */
+    PRIMARY KEY (FS_ID)
+);
+
+CREATE TABLE rb_backup
+(
+    VOL_ID INTEGER NOT NULL,
+    FS_ID INTEGER NOT NULL,
+    BACKUP_DT TIMESTAMP, /* time backup completed */
+    RB_NAME VARCHAR,	 /* backup name, usually looks like a date: YYMmmDD */
+    PRIMARY KEY (VOL_ID,FS_ID,RB_NAME)
+);
