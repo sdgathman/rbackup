fb6da9ceff22d21e2b610932717d42a6771a9a3a
Stuart D. Gathman <stuart@gathman.org>
2023-01-28 19:25:27 -0500

    Open crypt device after formatting

--- luksfmt.sh
+++ luksfmt.sh
@@ -17,7 +17,12 @@ header="/var/lib/rbackup/$label.luks"
 test -s "$header" && die "$label: header already exists"
 
 cryptsetup --header "$header" luksFormat "$device"
+# luksFormat does not open the device
+# entering passphrase a 3rd time is a feature, not a bug :-)
+cryptsetup --header "$header" open "$device" "$label"
 
+# EL8 does not support btrfs, so XFS is a good common format.  It does not
+# suffer from hard link count exhaustion like EXT4.
 test -b "/dev/mapper/$label" && mkfs.xfs -L "$label" "/dev/mapper/$label"
 
 mount "/dev/mapper/$label" "$media" || die "$label: can't mount"
